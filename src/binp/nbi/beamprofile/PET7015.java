f4c66c2c0d0dba380ec614d40a0e5d596cc918a8
sanin <sanin@MyDySo>
2016-07-09 22:52:03 +0600

    Merge origin/master
    Conflicts:
    	build.xml
    	manifest.mf
    	nbproject/build-impl.xml
    	nbproject/genfiles.properties
    	nbproject/project.properties
    	nbproject/project.xml

--- src/binp/nbi/beamprofile/PET7015.java
+++ src/binp/nbi/beamprofile/PET7015.java
@@ -11,8 +11,11 @@ import java.util.logging.Level;
 import java.util.logging.Logger;
 import net.wimpi.modbus.Modbus;
 import net.wimpi.modbus.ModbusException;
+import net.wimpi.modbus.ModbusIOException;
 import net.wimpi.modbus.ModbusSlaveException;
 import net.wimpi.modbus.io.ModbusTCPTransaction;
+import net.wimpi.modbus.io.ModbusTransaction;
+import net.wimpi.modbus.msg.ModbusResponse;
 import net.wimpi.modbus.msg.ReadCoilsRequest;
 import net.wimpi.modbus.msg.ReadCoilsResponse;
 import net.wimpi.modbus.msg.ReadInputDiscretesRequest;
@@ -21,7 +24,16 @@ import net.wimpi.modbus.msg.ReadInputRegistersRequest;
 import net.wimpi.modbus.msg.ReadInputRegistersResponse;
 import net.wimpi.modbus.msg.ReadMultipleRegistersRequest;
 import net.wimpi.modbus.msg.ReadMultipleRegistersResponse;
+import net.wimpi.modbus.msg.WriteCoilRequest;
+import net.wimpi.modbus.msg.WriteMultipleCoilsRequest;
+import net.wimpi.modbus.msg.WriteMultipleCoilsResponse;
+import net.wimpi.modbus.msg.WriteMultipleRegistersRequest;
+import net.wimpi.modbus.msg.WriteMultipleRegistersResponse;
+import net.wimpi.modbus.msg.WriteSingleRegisterRequest;
 import net.wimpi.modbus.net.TCPMasterConnection;
+import net.wimpi.modbus.procimg.Register;
+import net.wimpi.modbus.procimg.SimpleRegister;
+import net.wimpi.modbus.util.BitVector;
 
 /**
  *
@@ -50,9 +62,9 @@ public class PET7015 {
 "0x81: Platinum 100, α=0. 003916, -200°C ~ 600°C",
 "0x82: Cu 50 @ 0°C, -50°C ~ 150°C",
 "0x83: Nickel 100, -60°C ~ 180°C"};
-    static final int[] types = { 0x20, 0x21, 0x22, 0x23, 0x24, 0x25, 0x26, 
-        0x27, 0x28, 0x29, 0x2A, 0x2B, 0x2C, 0x2D, 0x2E, 0x2F, 0x80, 0x81,
-        0x82, 0x83};
+    static final int[] types =   { 0x20,   0x21,   0x22,   0x23,   0x24,   0x25,   0x26, 
+        0x27,   0x28,   0x29,   0x2A,   0x2B,   0x2C,   0x2D,   0x2E,   0x2F,   0x80,   0x81,
+        0x82,   0x83  };
     static final double[] pmin = { -100.0, -100.0, -200.0, -600.0, -100.0, -100.0, -200.0, 
         -600.0, -100.0, -100.0, -600.0, -150.0, -200.0, -150.0, -200.0, -200.0, -600.0, -600.0,
         -150.0, -180.0};
@@ -60,11 +72,9 @@ public class PET7015 {
          600.0,  100.0,  100.0,  600.0,  150.0,  200.0,  150.0,  200.0,  200.0,  600.0,  600.0,
          150.0,  180.0};
     
-    TCPMasterConnection con = null;         //the connection
-    ModbusTCPTransaction trans = null;      //the transaction
-    //ReadInputDiscretesRequest req = null; //the request
-    //ReadInputDiscretesResponse res = null; //the response
-    InetAddress addr = null; //the slave's address
+    TCPMasterConnection con = null;     //the connection
+    ModbusTransaction trans = null;     //the transaction
+    InetAddress addr = null;            //the slave's address
     int port = Modbus.DEFAULT_PORT;
     int unitID = 1; //the unit identifier we will be talking to
     int ref = 0;    //the reference; offset where to start reading from
@@ -74,11 +84,11 @@ public class PET7015 {
     int[] channels = new int[7];
     int[] cti = new int[7];
     
-    PET7015(String strAddr, int port) throws UnknownHostException, Exception {
+    public PET7015(String strAddr, int port) throws UnknownHostException, Exception {
         setInetAddress(strAddr);
         setPort(port);
-        con = new TCPMasterConnection(addr);
         openConnection();
+
         moduleName = readMultipleRegisters(559, 1)[0];
         if(moduleName != 0x7015) {
             System.out.printf("Incorrect module name: 0x%H\n", moduleName);
@@ -104,19 +114,24 @@ public class PET7015 {
         }
     }
     
-    PET7015(String strAddr) throws UnknownHostException, Exception {
+    public PET7015(String strAddr) throws UnknownHostException, Exception {
         this(strAddr, Modbus.DEFAULT_PORT);
     }
 
-    void setInetAddress(String strAddr) throws UnknownHostException {
+    void setInetAddress(String strAddr) throws UnknownHostException, Exception {
         addr  = InetAddress.getByName(strAddr);
+        closeConnection();
+        openConnection();
     }
     
-    void setPort(int newPort) {
+    void setPort(int newPort) throws Exception {
         port  = newPort;
+        closeConnection();
+        openConnection();
     }
     
     void openConnection() throws Exception {
+        con = new TCPMasterConnection(addr);
         con.setPort(port);
         con.connect();
     }
@@ -124,69 +139,116 @@ public class PET7015 {
     void closeConnection() {
         con.close();
     }
+    
+    boolean isConnected() {
+        return con.isConnected();
+    }
 
-    int[] readMultipleRegisters(int ref, int count) throws ModbusSlaveException, ModbusException {
-        //3. Prepare the request
+    ModbusResponse getResponse() {
+        return trans.getResponse();
+    }
+    
+    public int[] readMultipleRegisters(int ref, int count) throws ModbusSlaveException, ModbusException {
         ReadMultipleRegistersRequest req = new ReadMultipleRegistersRequest(ref, count);
         req.setUnitID(unitID);
-        //req.setHeadless();
-        //4. Prepare the transaction
         trans  = new ModbusTCPTransaction(con);
         trans.setRequest(req);
-        //5. Execute the transaction
         trans.execute();
         ReadMultipleRegistersResponse res = (ReadMultipleRegistersResponse) trans.getResponse();
-        int[] result = new int[res.getWordCount()];
-        for (int n = 0; n < res.getWordCount(); n++) {
-            result[n] = res.getRegisterValue(n);
+
+        int[] registers = new int[res.getWordCount()];
+        for (int i = 0; i < res.getWordCount(); i++) {
+            registers[i] = res.getRegisterValue(i);
         }
-        return result;
+        return registers;
     }
 
-    int[] readInputRegisters(int ref, int count) throws ModbusSlaveException, ModbusException {
-        //3. Prepare the request
+    public int[] readInputRegisters(int ref, int count) throws ModbusSlaveException, ModbusException {
         ReadInputRegistersRequest req = new ReadInputRegistersRequest(ref, count);
         req.setUnitID(unitID);
-        //req.setHeadless();
-        //4. Prepare the transaction
         trans  = new ModbusTCPTransaction(con);
         trans.setRequest(req);
-        //5. Execute the transaction
         trans.execute();
         ReadInputRegistersResponse res = (ReadInputRegistersResponse) trans.getResponse();
-        int[] result = new int[res.getWordCount()];
-        for (int n = 0; n < res.getWordCount(); n++) {
-            result[n] = res.getRegisterValue(n);
+
+        int[] registers = new int[res.getWordCount()];
+        for (int i = 0; i < res.getWordCount(); i++) {
+            registers[i] = res.getRegisterValue(i);
         }
-        return result;
+        return registers;
     }
 
-    boolean[] readCoils(int ref, int count) throws ModbusSlaveException, ModbusException {
+    public boolean[] readCoils(int ref, int count) throws ModbusSlaveException, ModbusException {
         ReadCoilsRequest req = new ReadCoilsRequest(ref, count);
         req.setUnitID(unitID);
         trans  = new ModbusTCPTransaction(con);
         trans.setRequest(req);
         trans.execute();
         ReadCoilsResponse res = (ReadCoilsResponse) trans.getResponse();
-        boolean[] result = new boolean[res.getBitCount()];
-        for (int n = 0; n < res.getBitCount(); n++) {
-            result[n] = res.getCoilStatus(n);
+
+        boolean[] coils = new boolean[res.getBitCount()];
+        for (int i = 0; i < res.getBitCount(); i++) {
+            coils[i] = res.getCoilStatus(i);
         }
-        return result;
+        return coils;
     }
 
-    boolean[] readInputDiscretes(int ref, int count) throws ModbusSlaveException, ModbusException {
+    public boolean[] readInputDiscretes(int ref, int count) throws ModbusSlaveException, ModbusException {
         ReadInputDiscretesRequest req = new ReadInputDiscretesRequest(ref, count);
         req.setUnitID(unitID);
         trans  = new ModbusTCPTransaction(con);
         trans.setRequest(req);
         trans.execute();
         ReadInputDiscretesResponse res = (ReadInputDiscretesResponse) trans.getResponse();
-        boolean[] result = new boolean[res.getBitCount()];
-        for (int n = 0; n < res.getBitCount(); n++) {
-            result[n] = res.getDiscreteStatus(n);
+
+        boolean[] bits = new boolean[res.getBitCount()];
+        for (int i = 0; i < res.getBitCount(); i++) {
+            bits[i] = res.getDiscreteStatus(i);
         }
-        return result;
+        return bits;
+    }
+
+    public void write(int ref, int[] values) throws  ModbusIOException, ModbusSlaveException, ModbusException {
+        Register[] regs = new SimpleRegister[values.length];
+        for(int i=0; i < values.length; i++) {
+            regs[i] = new SimpleRegister(values[i]);
+        }
+        WriteMultipleRegistersRequest req = new WriteMultipleRegistersRequest(ref, regs);
+        req.setUnitID(unitID);
+        trans  = new ModbusTCPTransaction(con);
+        trans.setRequest(req);
+        trans.execute();
+        //WriteMultipleRegistersResponse res = (WriteMultipleRegistersResponse) trans.getResponse();
+    }
+
+    public void write(int ref, boolean[] values) throws  ModbusIOException, ModbusSlaveException, ModbusException {
+        BitVector bv = new BitVector(values.length);
+        for(int i=0; i < values.length; i++) {
+            bv.setBit(i, values[i]);
+        }
+        WriteMultipleCoilsRequest req = new WriteMultipleCoilsRequest(ref, bv);
+        req.setUnitID(unitID);
+        trans  = new ModbusTCPTransaction(con);
+        trans.setRequest(req);
+        trans.execute();
+        //WriteMultipleCoilsResponse res = (WriteMultipleCoilsResponse) trans.getResponse();
+    }
+
+    public void write(int ref, boolean value) throws  ModbusIOException, ModbusSlaveException, ModbusException {
+        WriteCoilRequest req = new WriteCoilRequest(ref, value);
+        req.setUnitID(unitID);
+        trans  = new ModbusTCPTransaction(con);
+        trans.setRequest(req);
+        trans.execute();
+    }
+
+    public void write(int ref, int value) throws  ModbusIOException, ModbusSlaveException, ModbusException {
+        Register reg = new SimpleRegister(value);
+        WriteSingleRegisterRequest req = new WriteSingleRegisterRequest(ref, reg);
+        req.setUnitID(unitID);
+        trans  = new ModbusTCPTransaction(con);
+        trans.setRequest(req);
+        trans.execute();
     }
 
     public double[] read() {
@@ -195,9 +257,8 @@ public class PET7015 {
             int[] registers = readInputRegisters(0, 7);
             for (int i = 0; i < registers.length; i++) {
                 int index = cti[i];
-                int value = registers[i];
-                result[i] = pmax[index]*value/0x7fff;
-                if(value == 0x8000) result[i] = -9999.9;
+                result[i] = pmax[index]/0x7fff*registers[i];
+                if(registers[i] == 0x8000) result[i] = -9999.9;
             }
             return result;
         } catch (ModbusException ex) {

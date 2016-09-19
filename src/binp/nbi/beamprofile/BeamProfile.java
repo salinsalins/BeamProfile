6bcdcc903b235a653b34381fadecc91710b15dc5
sanin <sanin@MyDySo>
2016-09-19 20:51:26 +0700

    Merge origin/ModbusTest into ModbusTest
    Conflicts:
    	src/binp/nbi/beamprofile/BeamProfile.form
    	src/binp/nbi/beamprofile/BeamProfile.java

--- src/binp/nbi/beamprofile/BeamProfile.java
+++ src/binp/nbi/beamprofile/BeamProfile.java
@@ -1938,11 +1938,12 @@ public class BeamProfile extends javax.swing.JFrame implements WindowListener {
                         // Calculate data minimum and maximum
                         for (int i = 1; i < temp.length; i++) {
                             if (temp[i] > 0.0 && dmin[i] > temp[i]) {
-                                // !! Only for temperature channels 1 - 16                                if (i < 17 ) dmin[0] = temp[0];
+                                // !! Only for temperature channels 1 - 16
+                                if (i < 17 ) dmin[0] = temp[0];
                                 dmin[i] = temp[i];
                             }
                             if (dmax[i] < temp[i]) {
-                                // !! Only for temperature channels 1 - 16                                if (i < 17 ) dmin[0] = temp[0];
+                                // !! Only for temperature channels 1 - 16
                                 if (i < 17 ) dmax[0] = temp[0];
                                 dmax[i] = temp[i];
                             }
